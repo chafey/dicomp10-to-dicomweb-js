2ec24281a20d0c70b280e70118f82ff4611a13a6
Bill Wallace <wayfarer3130@gmail.com>
2022-02-09 20:06:16 -0500

    feat(staticwado):Basic working OHIF client/dicomweb static server

--- bin/dicomwebserver.mjs
+++ bin/dicomwebserver.mjs
@@ -4,4 +4,8 @@ import DicomWebServer from '../src/webserver/index.mjs'
 
 const server = DicomWebServer();
 
+server.addDicomWeb('/users/wayfa/dicomweb');
+server.addDicomWeb('/src/Viewers/testdata', {path: '/testdata'});
+server.addClient('/src/Viewers/platform/viewer/dist')
+
 server.listen();
--- src/webserver/index.mjs
+++ src/webserver/index.mjs
@@ -1,4 +1,57 @@
 import express from 'express'
+import logger from 'morgan'
+
+
+const qidoMap = (req,res,next) => {
+    console.log('QIDO request', req.path, req.url);
+    req.url = req.path + '/index.json.gz'
+    res.setHeader('content-type', 'application/json');
+    next();
+};
+
+const qidoMapPost = (req,res,next) => {
+    console.log('QIDO request post', req.path);
+    next()
+};
+
+const gzipHeaders = (res, path, stat) => {
+  if (path.indexOf('.gz') !== -1) {
+    res.setHeader('Content-Encoding', 'gzip')
+  } else if (path.indexOf('.br') !== -1) {
+    res.setHeader('Content-Encoding', 'br')
+  }
+};
+
+const methods = {
+
+    addDicomWeb: function(dir, params = {}) {
+        const path = params.path || '/dicomweb';
+        const router = express.Router();
+        this.use(path,router);
+
+        router.get('/studies', qidoMap);
+        router.get('/studies/:studyUID/series',qidoMap);
+        router.get('/studies/:studyUID/series/:seriesUID/instances',qidoMap);
+        // Handle the QIDO queries
+        router.use(express.static(dir, {
+            index: 'index.json.gz',
+            setHeaders: gzipHeaders,
+            extensions: ['gz'],
+            redirect: false,
+        }));
+    },
+
+    addClient: function(dir, params = {}) {
+        const path = params.path || '/';
+        
+        this.use(express.static(dir, {
+            index: 'index.html',
+            setHeaders: gzipHeaders,
+            extensions: ['gz'],
+            redirect: false,
+        }))
+    },
+};
 
 // Serve up the web files
 // Configuration is broken up into several parts:
@@ -10,18 +63,20 @@ import express from 'express'
 const DicomWebServer = (params) => {
     const app = express();
 
+    app.use(logger("combined"))
     app.params = params || {};
 
     const superListen = app.listen;
     app.listen = (port) => {
         if( port ) superListen.call(app,port);
-        superListen.call(app,app.params.port | 5000);
+        else {
+            console.log(`Server listening on ${app.params.port || 5000}`)
+            superListen.call(app,app.params.port || 5000);
+        }
     }
 
-    app.get('/', function (req, res) {
-        res.send('Hello World')
-      })
-    
+    Object.assign(app,methods);
+
     return app;
 }
 
